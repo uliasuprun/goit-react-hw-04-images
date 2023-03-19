2b16b6c5df66e0e2628dd0e0c433ac7c7cc3ac1c
uliasuprun <uliasuprun88@gmail.com>
2023-03-19 17:52:27 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,64 @@
 import toast, { Toaster } from 'react-hot-toast';
 
-import { Component } from 'react';
-
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
 import { MainWrapper } from './App.styled';
 
+
+import { useState, useEffect} from 'react';
+
 import { fetchImgGallery } from '../api/api';
-class App extends Component {
-  state = {
-    images: [],
-    nameRequest: '',
-    page: 1,
-    isLoading: false,
-    error: null,
-    totalHits: null,
-  };
 
-  getSearchNameForm = searchName => {
-    this.setState({
-      nameRequest: searchName,
-      page: 1,
-      images: [],
-      totalHits: null,
-    });
-  };
 
-  async componentDidUpdate(_, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.nameRequest !== this.state.nameRequest
-    ) {
-      this.setState({ isLoading: true });
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [nameRequest, setNameRequest] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(null);
+
+
+// class App extends Component {
+//   state = {
+//     images: [],
+//     nameRequest: '',
+//     page: 1,
+//     isLoading: false,
+//     error: null,
+//     totalHits: null,
+//   };
+
+const getSearchNameForm = searchName => {
+  setNameRequest(searchName);
+  setPage(1);
+  setImages([]);
+  setTotalHits(null);
+};
+
+  // getSearchNameForm = searchName => {
+  //   this.setState({
+  //     nameRequest: searchName,
+  //     page: 1,
+  //     images: [],
+  //     totalHits: null,
+  //   });
+  // };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    if (nameRequest === '') {
+      return;
+    }
+
+    async function getImgGallery() {
+      setIsLoading(true);
       try {
-        const images = await fetchImgGallery(
-          this.state.nameRequest,
-          this.state.page
-        );
+        const imgGallery = await fetchImgGallery(nameRequest, page, controller);
 
-        const arrImages = images.hits.map(
+        const arrImages = imgGallery.hits.map(
           ({ id, webformatURL, tags, largeImageURL }) => ({
             id,
             webformatURL,
@@ -49,55 +67,59 @@ class App extends Component {
           })
         );
 
-        if (images.totalHits === 0) {
+        if (imgGallery.totalHits === 0) {
           return toast.error('Sorry, didn`t find, try another');
         }
-        if (this.state.page >= 2) {
-          return this.setState({
-            images: [...prevState.images, ...arrImages],
-            totalHits: images.totalHits,
-          });
+        if (page >= 2) {
+          return (
+            setImages(images => [...images, ...arrImages]),
+            setTotalHits(imgGallery.totalHits)
+          );
         }
-
-        this.setState({
-          images: arrImages,
-          totalHits: images.totalHits,
-        });
+        setImages(arrImages);
+        setTotalHits(imgGallery.totalHits);
       } catch (error) {
-        this.setState({ error });
+        console.log(error.message);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
     }
-  }
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+    getImgGallery();
+    return () => {
+      controller.abort();
+    };
+  }, [nameRequest, page]);
+
+  const loadMore = () => {
+    setPage(page => page + 1);
   };
-  showBtnLoadMore = () => {
-    const ShowBtn = this.state.totalHits - this.state.page * 12;
-    if (ShowBtn > 0 && !this.state.isLoading) {
+
+  // loadMore = () => {
+  //   this.setState(prevState => ({
+  //     page: prevState.page + 1,
+  //   }));
+  // };
+
+  const showBtnLoadMore = () => {
+    const ShowBtn = totalHits - page * 12;
+    if (ShowBtn > 0 && !isLoading) {
       return true;
     }
     return false;
   };
-  render() {
-    const { isLoading, images, totalHits } = this.state;
 
-    return (
-      <MainWrapper>
-        <Searchbar onSubmit={this.getSearchNameForm} />
-        {/* <div>{isLoading && <div>Please wait... </div>}</div> */}
 
-        {totalHits > 0 ? <ImageGallery foundImages={images} /> : null}
+  return (
+    <MainWrapper>
+      <Searchbar onSubmit={getSearchNameForm} />
 
-        {isLoading && <Loader />}
-        {this.showBtnLoadMore() && <Button loadMore={this.loadMore} />}
-        <Toaster position="top-right" reverseOrder={false} />
-      </MainWrapper>
-    );
-  }
-}
+      {totalHits > 0 ? <ImageGallery foundImages={images} /> : null}
+      {isLoading && <Loader />}
+      {showBtnLoadMore() && <Button loadMore={loadMore} />}
+      <Toaster position="top-right" reverseOrder={false} />
+    </MainWrapper>
+  );
+};
+
 
 export default App;
